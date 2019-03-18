f9a03344f48b9aba14f8d5fdfc8e54711794f972
Charles Leon <charles.p.leon@gmail.com>
2019-03-17 20:53:33 -0700

    Change test to trigger build

--- test/test.js
+++ test/test.js
@@ -12,7 +12,7 @@ let server = require('../app');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+      assert.equal([1, 2, 5].indexOf(4), -1);
     });
   });
 });
